24ef9cec0f0012253d3ce1eae967b42696e55774
anikkhoma <anik123wb@gmail.com>
2024-01-02 00:43:04 +0200

    :sparkles: updates

--- mapper/product/map.productDocument.to.productDto.ts
+++ mapper/product/map.productDocument.to.productDto.ts
@@ -6,6 +6,7 @@ import { ProductDocument } from "../../schema/product.schema";
 export function mapProductDocumentToProductDto(
 	object: ProductDocument,
 	lang: LanguageEnum,
+	colorInFilters?: string[],
 ): ProductDto {
 	return {
 		id: object._id.toString(),
@@ -19,7 +20,32 @@ export function mapProductDocumentToProductDto(
 		quantity: object.quantity,
 		price: object.price,
 		discount: object.discount,
-		selectedColor: "no any color ololo",
+		selectedItem: (() => {
+			if (!colorInFilters) {
+				return object.items[0]?.sku ?? null;
+			}
+
+			// const bestItem = object.items.find((item) =>
+			// 	(item.attributes["color"] ?? []).some((color) => colorInFilters.includes(color)),
+			// );
+
+			const [bestItem] = object.items
+				.flatMap((item) =>
+					colorInFilters.map((color) => ({
+						score: (item.attributes["color"] ?? []).indexOf(color),
+						item,
+					})),
+				)
+				.filter((item) => item.score !== -1)
+				.map((score) => ({ ...score, score: score.score }))
+				.sort((a, b) => a.score - b.score);
+
+			if (bestItem) {
+				return bestItem.item.sku;
+			}
+
+			return null;
+		})(),
 		active: object.active,
 		createDate: "no any date ololo",
 	};
